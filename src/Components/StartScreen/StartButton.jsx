0b7e3d20aa53e7886c863fbb514964d008a52398
Ibrahim Farrah <ibrahimfarrah30@gmail.com>
2023-06-09 20:26:14 +0100

    final additions and updatesto the start page

--- src/Components/StartScreen/StartButton.jsx
+++ src/Components/StartScreen/StartButton.jsx
@@ -2,20 +2,36 @@
 import { AwesomeButton } from 'react-awesome-button';
 import 'react-awesome-button/dist/styles.css';
 import'./StartScreen.css';
-
+import { useState } from 'react';
 
 
 function StartButton() {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleHover = () => {
+    setIsHovered(true);
+  };
+
+  const handleHoverExit = () => {
+    setIsHovered(false);
+  };
 return(
 <>
 <div className="container">
-    <AwesomeButton  type="secondary" href='/Main'>Start</AwesomeButton>
-</div>
-
-<b className="textContainer">
-<span className='text1'>About:</span>
-<span className='text2'>Find all 10 buttons to finnish the game</span>
-</b>
+        <div
+          className={`start-container ${isHovered ? 'hovered' : ''}`}
+          onMouseEnter={handleHover}
+          onMouseLeave={handleHoverExit}
+        >
+          <AwesomeButton type="secondary" href="/Main">
+            Start
+          </AwesomeButton>
+          <div className="textContainer">
+          <span className="title">Game Objective:</span>
+            <span className="text"> Explore numerous pages and find all 10 buttons.</span>
+          </div>
+        </div>
+      </div>
 </>
   
 );
