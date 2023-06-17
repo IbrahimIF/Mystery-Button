e67f2bb6655afe455271ccb885a6f49fe8e8784f
Ibrahim Farrah <ibrahimfarrah30@gmail.com>
2023-06-17 22:37:36 +0100

    fixed the container flex display problem so that it fits within the button and position of button was changed

--- src/Components/MainScreen/MainScreen.jsx
+++ src/Components/MainScreen/MainScreen.jsx
@@ -1,20 +1,21 @@
-
-import "./MainScreen.css"
-import {Link } from "react-router-dom";
+import "./MainScreen.css";
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
 
 function MainScreen() {
-  return (
-  <>
-  <div className="flip">
-  <div className="buttonContainer">
-      <div className="baseButton">
-      <Link to="/Random">  <button className="button" to="/page2"><p className="text">Button</p></button>
-      </Link>
-        <div className="buttonBellow"></div>
+   return (
+    <>
+    <div className="buttonContainer">
+        <div className="baseButton">
+          <Link to="/Random">
+            <button className="button">
+              <p className="text">Button</p>
+            </button>
+          </Link>
+          <div className="buttonBellow"></div>
+        </div>
       </div>
-    </div>
-  </div>
-  </>
+    </>
   );
 }
 
